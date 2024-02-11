bd97563de5bb3c96ac5f334534607f4e4cad1a52
Ryan Ching <chingry@uw.edu>
2024-02-11 14:11:01 -0800

    added keypress functions

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const inputRef = useRef(null);
   const resultRef = useRef(null);
   const [result, setResult] = useState(0);
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if (/^[0-9]$/.test(e.key)) {
+        inputRef.current.value =
+          inputRef.current.value == 0 ? e.key : inputRef.current.value + e.key;
+      } else if (e.key == "*" || e.key == "x") {
+        e.preventDefault();
+        setResult((result) => result * Number(inputRef.current.value));
+      } else if (e.key == "/") {
+        e.preventDefault();
+        setResult((result) => result / Number(inputRef.current.value));
+      } else if (e.key == "+") {
+        e.preventDefault();
+        setResult((result) => result + Number(inputRef.current.value));
+      } else if (e.key == "-") {
+        e.preventDefault();
+        setResult((result) => result - Number(inputRef.current.value));
+      } else if (e.key == "c") {
+        e.preventDefault();
+        inputRef.current.value = "";
+      } else if (e.key == "v") {
+        e.preventDefault();
+        setResult((result) => 0);
+      } else if (e.key == "b") {
+        e.preventDefault();
+        inputRef.current.value = "";
+        setResult((result) => 0);
+      }
+    };
+    document.addEventListener("keypress", handleKeyPress);
+    return () => {
+      document.removeEventListener("keypress", handleKeyPress);
+    };
+  }, []);
 
   function plus(e) {
     e.preventDefault();
@@ -28,7 +64,7 @@ function App() {
 
   function resetInput(e) {
     e.preventDefault();
-    inputRef.current.value = 0;
+    inputRef.current.value = "";
   }
 
   function resetResult(e) {
@@ -47,8 +83,10 @@ function App() {
         <h1>Calculator</h1>
       </div>
       <form>
+        <h2>Input</h2>
+        <input ref={inputRef} type="text" />
+        <h2>Result</h2>
         <p ref={resultRef}>{result}</p>
-        <input pattern="[0-9]" ref={inputRef} type="number" placeholder="Type a number" />
         <div className="all-buttons">
           <button onClick={(e) => append(7, e)}>7</button>
           <button onClick={(e) => append(8, e)}>8</button>
